5793101bcaef78e92c535a7148779a3977b4a2af
iceWhispers <55951828+iceWhispers@users.noreply.github.com>
2025-05-10 16:26:12 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function entrar() {
-  const nome = document.getElementById('inputName').value.toLowerCase();
-  if (nome === 'icewhispers' || nome === 'soneca') {
-    document.getElementById('menu').classList.remove('hidden');
-  } else {
-    alert('O véu permanece fechado para quem não conhece o nome.');
-  }
-}
